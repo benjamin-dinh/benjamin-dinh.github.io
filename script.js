41ca4e49b27723c5f32e89316b31bf6a695d3a88
Benjamin Dinh <bhdinh@ucdavis.edu>
2020-12-08 05:54:13 -0800

    dark mode 🌚

--- script.js
+++ script.js
@@ -28,6 +28,8 @@ name.addEventListener('click',(e)=>{
   window.scrollTo({ top: 0, behavior: 'smooth' });
 });
 
+// DARK THEME TOGGLER
+// credit to: https://dev.to/ananyaneogi/create-a-dark-light-mode-switch-with-css-variables-34l8
 const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
 function switchTheme(e) {
     if (e.target.checked) {
@@ -49,7 +51,7 @@ function switchTheme(e) {
   }    
 }
 
-const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+const currentTheme = localStorage.getItem('theme');
 if (currentTheme) {
     document.documentElement.setAttribute('data-theme', currentTheme);
 
